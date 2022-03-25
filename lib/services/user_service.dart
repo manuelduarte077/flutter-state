0e2b3e1adcac1a768a002336dbbf4d0926ae2b5c
Manuel Duarte <carlosd99m@gmail.com>
2022-03-25 12:43:12 -0600

    refactor models

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -4,11 +4,7 @@ class _UserService {
   late User _userInfo;
 
   User get userInfo => _userInfo;
-
-  final User _user = User(name: 'John Doe');
-
   // Para saber si el usuario esta logeado
-  bool get existeUsuario => (_user != null);
 
   // Cargar el usuario
   void getAllUser(User usuario) {
