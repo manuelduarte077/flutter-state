0df31c1d0c48d89f69b45bb49addae06aa9e2aef
Manuel Duarte  <carlosd99m@gmail.com>
2023-06-14 23:56:20 -0600

    form validation

--- forms_app/lib/presentation/screens/home_screen.dart
+++ forms_app/lib/presentation/screens/home_screen.dart
@@ -25,6 +25,12 @@ class HomeScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 10),
             child: Divider(),
           ),
+          ListTile(
+            title: const Text('Nuevo usuario'),
+            subtitle: const Text('Manejo de formularios'),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+            onTap: () => context.push('/new-user'),
+          ),
         ],
       ),
     );
---
+++ forms_app/lib/presentation/screens/register_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/register/register_cubit.dart';
+
+import '../widgets/inputs/custom_text_form_field.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nuevo usuario'),
+      ),
+      body: BlocProvider(
+        create: (context) => RegisterCubit(),
+        child: const _RegisterView(),
+      ),
+    );
+  }
+}
+
+class _RegisterView extends StatelessWidget {
+  const _RegisterView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              FlutterLogo(size: 100),
+              _RegisterForm(),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RegisterForm extends StatelessWidget {
+  const _RegisterForm();
+
+  @override
+  Widget build(BuildContext context) {
+    final registerCubit = context.watch<RegisterCubit>();
+    final username = registerCubit.state.username;
+    final email = registerCubit.state.email;
+    final password = registerCubit.state.password;
+
+    return Form(
+      child: Column(
+        children: [
+          CustomTextFormField(
+            label: 'Nombre de usuario',
+            onChanged: registerCubit.usernameChanged,
+            errorMessage: username.errorMessage,
+          ),
+          const SizedBox(height: 10),
+          CustomTextFormField(
+            label: 'Correo electrónico',
+            onChanged: registerCubit.emailChanged,
+            errorMessage: email.errorMessage,
+          ),
+          const SizedBox(height: 10),
+          CustomTextFormField(
+            label: 'Contraseña:',
+            obscureText: true,
+            onChanged: registerCubit.passwordChanged,
+            errorMessage: password.errorMessage,
+          ),
+          const SizedBox(height: 20),
+          FilledButton.tonalIcon(
+            onPressed: () {
+              registerCubit.onSubmit();
+            },
+            icon: const Icon(Icons.save),
+            label: const Text('Crear usuario'),
+          ),
+        ],
+      ),
+    );
+  }
+}
